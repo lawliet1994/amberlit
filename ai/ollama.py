13d2fe95bac2df7790564aa145964e55b6b19aa1
= <=>
2025-01-06 20:35:22 +0800

    feat(ai): 为 ollama 模型添加流式输出功能
    - 在 secrets.toml 中添加 stream_delay 参数，用于控制流式输出的延迟
    - 修改 ollama.py 文件，增加流式输出的相关逻辑和界面选项
    - 优化消息展示函数，支持流式输出

--- ai/ollama.py
+++ ai/ollama.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import sys
+import sys,time
 import ollama
 from ifunctions import Message
 
@@ -12,13 +12,20 @@ def display_message(mt:dict,is_stream=False):
     '''展示用户与ollama的聊天框'''
     with st.chat_message(mt['role']):
         if is_stream:
-            st.write_stream(mt['content'])
+            return st.write_stream(mt['content'])
         else:
             st.write(mt['content'])
-
+def stream_chunk(chunks):
+    for chunk in chunks:
+        time.sleep(st.secrets.ollama.stream_delay)
+        yield chunk['message']['content']
+# 侧边栏
+with st.sidebar:
+    is_stream = st.checkbox('流式响应',value=False)
+    st.write(is_stream)
 # 主界面
 st.title('ollama test')
-
+# 显示历史记录
 for message in st.session_state['ollama_history']:
     display_message(mt=message)
 
@@ -26,11 +33,13 @@ question = st.chat_input("question")
 if question:
     display_message(mt = mt.user_mesasage(question))# 显示用户提问
     st.session_state['ollama_history'].append(mt.user_mesasage(question))# 保存用户提问
-    response = o.chat(
-        model=st.secrets.ollama.model,
-        #messages=[mt.user_mesasage(question)],
-        messages=st.session_state['ollama_history'],
-        stream=False,
-    )['message']['content']
-    display_message(mt=mt.assistant_mesasage(response))# 显示ollama的回答
+    temp_response = o.chat(model=st.secrets.ollama.model,messages=st.session_state['ollama_history'],stream=is_stream,)#['message']['content']# 获取ollama的回答
+    if is_stream:
+        meta_stream_text = stream_chunk(temp_response)# 流式输出
+        response = display_message(mt=mt.assistant_mesasage(meta_stream_text),is_stream=is_stream)# 显示ollama的回答
+        
+
+    else:
+        response = temp_response['message']['content']
+        display_message(mt=mt.assistant_mesasage(response))# 显示ollama的回答
     st.session_state['ollama_history'].append(mt.assistant_mesasage(response))# 保存ollama的回答

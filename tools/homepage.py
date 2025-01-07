62ac1e09755143213abf7b80df17b0f98bf35dd0
= <=>
2025-01-07 21:58:12 +0800

    feat(tools): 更新 homepage.py 以显示实时天气信息
    - 使用 jinja2 模板引擎渲染 HTML
    - 从 Baidu API 获取实时天气数据
    - 在页面上显示当前温度、湿度、风速和更新时间
    - 调整 HTML 样式，注释掉背景颜色

--- tools/homepage.py
+++ tools/homepage.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import sys
 from ifunctions import Baidu
+from jinja2 import Template
+import streamlit.components.v1 as components
+import datetime
+
 st.set_page_config(layout='wide')
 baidu = Baidu(st.secrets.baidu.API_KEY)
 st.title("Homepage")
@@ -10,8 +14,8 @@ weather = baidu.get_weather(district_id=310100).json()
 # st.write(weather['result']['location'])
 # st.write(weather['result']['now'])
 # st.write(weather['result']['forecasts'])
-
-st.html("""    
+st.write(weather['result']['now'])
+html_str = """    
 <!DOCTYPE html>
 <html lang="zh-CN">
 <head>
@@ -24,7 +28,7 @@ st.html("""
             justify-content: center;
             align-items: center;
             height: 100vh;
-            background-color: #f0f8ff;
+            {# background-color: #f0f8ff; #}
             margin: 0;
             font-family: Arial, sans-serif;
         }
@@ -55,13 +59,20 @@ st.html("""
         <div class="weather-icon">☀️</div>
         <div class="temperature">7°C</div>
         <div class="details">
-            <p>体感温度: 6°C</p>
-            <p>湿度: 62%</p>
-            <p>风速: 1级 东北风</p>
-            <p>更新时间: 2025-01-06 21:40</p>
+            <p>体感温度: {{temp}}°C</p>
+            <p>湿度: {{rh}}%</p>
+            <p>风速: {{wind_class}} {{wind_dir}}</p>
+            <p>更新时间: {{update_time}}</p>
         </div>
     </div>
 </body>
 </html>     
         
-        """)
+        """
+html_temp = Template(html_str)
+html_final = html_temp.render(temp=weather['result']['now']['temp'],
+                             rh=weather['result']['now']['rh'],
+                             wind_class=weather['result']['now']['wind_class'],
+                             wind_dir=weather['result']['now']['wind_dir'],
+                             update_time=(weather['result']['now']['uptime']))
+components.html(html_final, height=350)

99a3e36d5389a9b32042e6d61701bca41847aa6f
= <=>
2025-01-09 20:36:00 +0800

    feat(ai): 添加通义千问模型支持
    - 在 secrets.toml 中添加了 qwen 模型的 API_KEY
    - 新增 qwen_web.py 文件，实现了与 qwen 模型的交互
    - 更新 note.ipynb，添加了使用 qwen-turbo 模型的示例代码
    - 优化了用户界面，支持选择不同模型并显示历史记录

--- ai/qwen_web.py
+++ ai/qwen_web.py
@@ -1,15 +1,63 @@
 import streamlit as st
 import ifunctions as ifs
-
+import random
+from http import HTTPStatus
+from dashscope import Generation
 # 初始化参数
 mg = ifs.Message()
+model_dict = {
+    'qwq-32b-preview':'QwQ 模型是由 Qwen 团队开发的实验性研究模型，专注于增强 AI 推理能力，尤其是数学和编程领域。',
+    'qwen-coder-plus':'Qwen 的代码模型',
+    'qwen-max':'适合复杂任务，推理能力最强',
+    'qwen-plus':'效果、速度、成本均衡',
+    'qwen-turbo':'适合简单任务，速度快、成本低',
+    'qwen-long':'持长达千万字文档，成本低',
+}
+model_list = model_dict.keys()
 if 'qwen_history' not in st.session_state:
     st.session_state['qwen_history'] = []
-
+def get_response(model,messages):
+    responses = Generation.call(model=model,
+                                messages=messages,
+                                # 设置随机数种子seed，如果没有设置，则随机数种子默认为1234
+                                seed=random.randint(1, 10000),
+                                # 将输出设置为"message"格式
+                                result_format='message',
+                                stream=True,
+                                incremental_output=True,
+                                api_key=st.secrets.qwen.API_KEY)
+    for response in responses:
+        if response.status_code == HTTPStatus.OK:
+            yield response.output.choices[0]['message']['content']
+    #return responses
+#############################################***************主界面***************#############################################
+# 侧边栏
+with st.sidebar:
+    st.title('参数设置')
+    model_selected = st.selectbox('选择模型',model_list)
+    if st.button('清除历史记录',type='primary'):
+        st.session_state['qwen_history'] = []
+# 主界面
 st.title('通义千问')
-st.caption('调用阿里云百炼 api ，悠着点用。')
+st.caption(f'调用阿里云百炼 api :{model_selected}，悠着点用。')
 
 temp_question = st.chat_input("question")
+
+# 展示历史记录
+for message in st.session_state['qwen_history']:
+    with st.chat_message(message['role']):
+        st.write(message['content'])
+
+if temp_question:
+    with st.chat_message("user"):
+        st.write(temp_question)
+    question = mg.user_mesasage(temp_question)
+    st.session_state['qwen_history'].append(question)
+    # for response in get_response(st.session_state['qwen_history']):
+    #     full_response = st.write(response)
+    with st.chat_message("assistant"):
+        full_response = st.write_stream(get_response(model=model_selected,messages=st.session_state['qwen_history']))
+    st.session_state['qwen_history'].append(mg.assistant_mesasage(full_response))
 
 
 
